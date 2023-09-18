dc29eb2a4342845233daa974463801773c3956b9
seung-ho back <ilikeapink@naver.com>
2023-09-18 10:51:49 +0000

    분해합

---
+++ beakjoon/2231.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    num = sum((map(int, str(i))))
+    num_sum = i + num
+    
+    if num_sum == N:
+        print(i)
+        break
+    if i == N:
+        print(0)

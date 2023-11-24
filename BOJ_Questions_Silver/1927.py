477ee2475ac3d1f9853339e466f28a9e20f06768
seung-ho back <ilikeapink@naver.com>
2023-11-24 10:45:25 +0000

    BOJ_Solve

--- BOJ_Questions_Silver/1927.py
+++ BOJ_Questions_Silver/1927.py
@@ -1,3 +1,4 @@
+# 파이썬 heapq 모듈은 heapq (priority queue) 알고리즘을 제공함
 import heapq
 import sys
 input = sys.stdin.readline
@@ -7,10 +8,13 @@ heap = []
 for _ in range(N):
     x = int(input())
 
+    # 0을 입력받았을 때 heap에 최솟값 출력
     if x == 0:
         if heap:
             print(heapq.heappop(heap))
+        # heap이 비어있을 때 0 출력
         else:
             print('0')
+    # 0이 아닌 자연수를 입력받았을 때 heap에 저장
     else:
         heapq.heappush(heap, x)
